3a660213d5e7d2adbcbb41dada3b763dc73a5deb
coyote <megan.metelak@gmail.com>
2022-04-06 16:09:21 -0500

    test emoji and nav links

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Nav from "..";
+//declare what this test suite will be testing
+describe("Nav component", () => {
+  // baseline test
+  it("renders", () => {
+    render(<Nav />);
+  });
+  // snapshot test
+  it("matches snapshot", () => {
+    const { asFragment } = render(<Nav />);
+    // assert value comparison
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+//test if the camera emoji is visible
+describe("emoji is visible", () => {
+  it("inserts emoji into the h2", () => {
+    // Arrange
+    const { getByLabelText } = render(<Nav />);
+    // Assert
+    expect(getByLabelText("camera")).toHaveTextContent("📸");
+  });
+});
+//check to see if links are visible
+describe("links are visible", () => {
+  it("inserts text into the links", () => {
+    // Arrange
+    const { getByTestId } = render(<Nav />);
+    // Assert
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
+  });
+});
